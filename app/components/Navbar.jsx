7f9db669447623df6c1df7a7233e9a190187aa35
Terry Babyuk <tbabyuk@gmail.com>
2024-10-03 22:47:34 -0400

    fixed mobile nav submenu parent links, changed url structure to remove 'services' segment

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -20,31 +20,31 @@ export const Navbar = () => {
             </div>
             <nav className={`${dropdownIsOpen ? "block" : "hidden"} md:flex-1 md:flex justify-center items-center bg-g-moon-dark-gray z-20`}>
                 <ul className="flex flex-col md:flex-row h-full">
-                    <li><Link href="/" className="g-moon-nav-link hover:bg-g-moon-dark-gray-hover">HOME</Link></li>
-                    <li className="relative"><Link href="#" className="g-moon-nav-link" onClick={() => setServicesSubmenuOpen(!servicesSubmenuOpen)} onMouseEnter={() => setServicesSubmenuOpen(true)} onMouseLeave={() => setServicesSubmenuOpen(false)}>SERVICES <MdOutlineKeyboardArrowDown size="1.3rem" /></Link>
+                    <li><Link href="/" className="g-moon-nav-link hover:bg-g-moon-dark-gray-hover" onClick={() => setDropdownIsOpen(false)}>HOME</Link></li>
+                    <li className="relative"><span className="g-moon-nav-link" onClick={() => setServicesSubmenuOpen(!servicesSubmenuOpen)} onMouseEnter={() => setServicesSubmenuOpen(true)} onMouseLeave={() => setServicesSubmenuOpen(false)}>SERVICES <MdOutlineKeyboardArrowDown size="1.3rem" /></span>
                         {servicesSubmenuOpen && 
                             <ul className="bg-g-moon-dark-gray w-full md:w-fit md:absolute" onMouseEnter={() => setServicesSubmenuOpen(true)} onMouseLeave={() => setServicesSubmenuOpen(false)}>
-                                <li className="whitespace-nowrap"><Link href="/services/massages" className="g-moon-subnav-link hover:bg-g-moon-dark-gray-hover">Massages</Link></li>
-                                <li className="whitespace-nowrap"><Link href="/services/body-scrubs" className="g-moon-subnav-link">Body Scrubs</Link></li>
-                                <li className="whitespace-nowrap"><Link href="/services/table-shower" className="g-moon-subnav-link">Table Shower</Link></li>
-                                <li className="whitespace-nowrap"><Link href="/services/hot-stone-therapy" className="g-moon-subnav-link">Hot Stone Therapy</Link></li>
-                                <li className="whitespace-nowrap"><Link href="/services/aromatherapy" className="g-moon-subnav-link">Aromatherapy</Link></li>
-                                <li className="whitespace-nowrap"><Link href="/services/reflexology" className="g-moon-subnav-link">Reflexology</Link></li>
-                                <li className="whitespace-nowrap"><Link href="/services/consultation" className="g-moon-subnav-link">Consultation</Link></li>
+                                <li className="whitespace-nowrap"><Link href="/massages" className="g-moon-subnav-link hover:bg-g-moon-dark-gray-hover" onClick={() => setDropdownIsOpen(false)}>Massages</Link></li>
+                                <li className="whitespace-nowrap" ><Link href="/body-scrubs" className="g-moon-subnav-link" onClick={() => setDropdownIsOpen(false)}>Body Scrubs</Link></li>
+                                <li className="whitespace-nowrap"><Link href="/table-shower" className="g-moon-subnav-link" onClick={() => setDropdownIsOpen(false)}>Table Shower</Link></li>
+                                <li className="whitespace-nowrap"><Link href="/hot-stone-therapy" className="g-moon-subnav-link" onClick={() => setDropdownIsOpen(false)}>Hot Stone Therapy</Link></li>
+                                <li className="whitespace-nowrap"><Link href="/aromatherapy" className="g-moon-subnav-link" onClick={() => setDropdownIsOpen(false)}>Aromatherapy</Link></li>
+                                <li className="whitespace-nowrap"><Link href="/reflexology" className="g-moon-subnav-link" onClick={() => setDropdownIsOpen(false)}>Reflexology</Link></li>
+                                <li className="whitespace-nowrap"><Link href="/consultation" className="g-moon-subnav-link" onClick={() => setDropdownIsOpen(false)}>Consultation</Link></li>
                             </ul>
                         }
                     </li>
-                    <li className="relative"><Link href="#" className="g-moon-nav-link" onClick={() => setBookingSubmenuOpen(!bookingSubmenuOpen)}  onMouseEnter={() => setBookingSubmenuOpen(true)} onMouseLeave={() => setBookingSubmenuOpen(false)}>BOOKING <MdOutlineKeyboardArrowDown size="1.3rem" /></Link>
+                    <li className="relative"><span href="#" className="g-moon-nav-link" onClick={() => setBookingSubmenuOpen(!bookingSubmenuOpen)}  onMouseEnter={() => setBookingSubmenuOpen(true)} onMouseLeave={() => setBookingSubmenuOpen(false)}>BOOKING <MdOutlineKeyboardArrowDown size="1.3rem" /></span>
                         {bookingSubmenuOpen && 
                             <ul className="bg-g-moon-dark-gray w-full md:w-fit md:absolute" onMouseEnter={() => setBookingSubmenuOpen(true)} onMouseLeave={() => setBookingSubmenuOpen(false)}>
-                                <li className="whitespace-nowrap"><Link href="/resources/videos" className="g-moon-subnav-link">Contact Form</Link></li>
-                                <li className="whitespace-nowrap"><Link href="/resources/preferred-vendors" className="g-moon-subnav-link">Booking Calendar</Link></li>
+                                <li className="whitespace-nowrap"><Link href="/resources/videos" className="g-moon-subnav-link" onClick={() => setDropdownIsOpen(false)}>Contact Form</Link></li>
+                                <li className="whitespace-nowrap"><Link href="/resources/preferred-vendors" className="g-moon-subnav-link" onClick={() => setDropdownIsOpen(false)}>Booking Calendar</Link></li>
                             </ul>
                         }
                     </li>
-                    <li><Link href="/team" className="g-moon-nav-link hover:bg-g-moon-dark-gray-hover">TEAM</Link></li>
-                    <li><Link href="/pricing" className="g-moon-nav-link hover:bg-g-moon-dark-gray-hover">PRICING</Link></li>
-                    <li><Link href="/about" className="g-moon-nav-link hover:bg-g-moon-dark-gray-hover">ABOUT</Link></li>
+                    <li><Link href="/team" className="g-moon-nav-link hover:bg-g-moon-dark-gray-hover" onClick={() => setDropdownIsOpen(false)}>TEAM</Link></li>
+                    <li><Link href="/pricing" className="g-moon-nav-link hover:bg-g-moon-dark-gray-hover" onClick={() => setDropdownIsOpen(false)}>PRICING</Link></li>
+                    <li><Link href="/about" className="g-moon-nav-link hover:bg-g-moon-dark-gray-hover" onClick={() => setDropdownIsOpen(false)}>ABOUT</Link></li>
                 </ul>
             </nav>
         </div>
