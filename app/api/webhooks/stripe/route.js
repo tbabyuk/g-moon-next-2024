3b14b980a529b4542307a91dd3e0c12df0c3dc62
Terry Babyuk <tbabyuk@gmail.com>
2024-10-10 12:48:03 -0400

    modified webhook code, attempt 4

--- app/api/webhooks/stripe/route.js
+++ app/api/webhooks/stripe/route.js
@@ -8,11 +8,55 @@ export async function POST(req) {
 
     const type = requestBody.type;
     const amount = requestBody.data.object.amount;
-    const userClerkId = requestBody.data.object.metadata.userClerkId;
+    const secret_word = "Banana"
+    // const userClerkId = requestBody.data.object.metadata.userClerkId;
 
         console.log("logging whole requestBody from Stripe webhook:", requestBody)
         console.log("Logging type and amount:", type, amount)
-        console.log("logging userClerkId from Stripe Webhook:", userClerkId)
+        // console.log("logging userClerkId from Stripe Webhook:", userClerkId)
+
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "terry@strictlywebdev.ca",
+            pass: process.env.NODEMAILER_STRICTLY_PASS
+        }
+    })
+
+
+    const emailOptions =
+    {
+        from: "terry@strictlywebdev.ca",
+        to: "terry@strictlywebdev.ca",
+        subject: "New Stripe Transaction",
+        html: `
+                <strong>Type:</strong><br />
+                <small>${type}</small>
+                <hr>
+                <strong>Amount:</strong><br />
+                <small>${amount}</small>
+                <hr>
+                <strong>Secret Word:</strong><br />
+                <small>${secret_word}</small>
+                <hr>
+            `
+    }
+
+
+    try {
+        // throw new Error("Ooops, error with register form!!!! OOPS")
+        await transporter.sendMail(emailOptions);
+        console.log("try block fired in Node.js")
+        // return NextResponse.json({message: "new registration email sent successfully"}, {status: 200})
+    } catch (error) {
+        console.log("error in catch black on server:", error.message)
+        // return NextResponse.json({message: error.message}, {status: 500})
+    }
+
+
+
+
 
         // let numCreditsPurchased;
 
@@ -49,107 +93,5 @@ export async function POST(req) {
         //     }
         // }
     
-
-
     return NextResponse.json({message: "success"})
 }
-
-
-
-// export async function POST(request) {
-
-//     const {
-//             student_name, 
-//             parent_name, 
-//             phone, 
-//             email, 
-//             instrument, 
-//             lesson_type, 
-//             lesson_duration, 
-//             lesson_frequency, 
-//             preferred_days, 
-//             source, 
-//             comments
-//             } = await request.json()
-
-
-//     const transporter = nodemailer.createTransport({
-//         service: "gmail",
-//         auth: {
-//             user: "info@dacapomusic.ca",
-//             pass: process.env.NODEMAILER_INFO_PASS
-//         }
-//     })
-
-
-//     const lessonEmailOptions =
-//         {
-//             from: "info@dacapomusic.ca",
-//             to: "info@dacapomusic.ca",
-//             subject: "New Lesson Registration",
-//             html: `
-//                     <strong>Student Name</strong><br />
-//                     <small>${student_name}</small>
-//                     <hr>
-//                     <strong>Parent/Guardian Name</strong><br />
-//                     <small>${parent_name}</small>
-//                     <hr>
-//                     <strong>Phone</strong><br />
-//                     <small>${phone}</small>
-//                     <hr>
-//                     <strong>Email</strong><br />
-//                     <small>${email}</small>
-//                     <hr>
-//                     <strong>Instrument</strong><br />
-//                     <small>${instrument}</small>
-//                     <hr>
-//                     <strong>Lesson Type</strong><br />
-//                     <small>${lesson_type}</small>
-//                     <hr>
-//                     <strong>Lesson Duration</strong><br />
-//                     <small>${lesson_duration}</small>
-//                     <hr>
-//                     <strong>Lesson Frequency</strong><br />
-//                     <small>${lesson_frequency}</small>
-//                     <hr>
-//                     <strong>Lesson Days</strong><br />
-//                     <small>Mondays: ${preferred_days.monday}</small><br />
-//                     <small>Tuesdays: ${preferred_days.tuesday}</small><br />
-//                     <small>Wednesdays: ${preferred_days.wednesday}</small><br />
-//                     <small>Thursdays: ${preferred_days.thursday}</small><br />
-//                     <small>Saturdays: ${preferred_days.saturday}</small>
-//                     <hr>
-//                     <strong>Source</strong><br />
-//                     <small>${source}</small>
-//                     <hr>
-//                     <strong>Comments</strong><br />
-//                     <small>${comments}</small>
-//                     <hr>
-//                 `
-//         }
-
-
-//         if(instrument === "band") {
-//             try {
-//                 // throw new Error("Ooops, error with register form!!!! OOPS")
-//                 await transporter.sendMail(bandEmailOptions);
-//                 console.log("try block fired in Node.js")
-//                 return NextResponse.json({message: "new registration email sent successfully"}, {status: 200})
-//             } catch (error) {
-//                 console.log("error in catch black on server:", error.message)
-//                 return NextResponse.json({message: error.message}, {status: 500})
-//             }
-
-//         } else {
-//             try {
-//                 // throw new Error("Ooops, error with register form!!!! OOPS")
-//                 await transporter.sendMail(lessonEmailOptions);
-//                 console.log("try block fired in Node.js")
-//                 return NextResponse.json({message: "new registration email sent successfully"}, {status: 200})
-//             } catch (error) {
-//                 console.log("error in catch black on server:", error.message)
-//                 return NextResponse.json({message: error.message}, {status: 500})
-//             }
-//         }
-
-// }
