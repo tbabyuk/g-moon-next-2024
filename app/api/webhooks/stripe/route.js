e621a48c455882d617ca60c58305127120f3028c
Terry Babyuk <tbabyuk@gmail.com>
2024-10-11 00:50:59 -0400

    trying dynamic customer email in webhook

--- app/api/webhooks/stripe/route.js
+++ app/api/webhooks/stripe/route.js
@@ -10,7 +10,7 @@ export async function POST(req) {
 
 
     // const amountTotal = requestBody.data.object.amount_total;
-    // const customerEmail = requestBody.data.object.customer_details.email
+    const customerEmail = requestBody.data.object.customer_details.email
     const customerName = requestBody.data.object.customer_details.name
     const serviceName = requestBody.data.object.metadata.chosenService
     const serviceDate = requestBody.data.object.metadata.chosenDate
@@ -41,15 +41,16 @@ export async function POST(req) {
     const mailOptions =
         {
             from: "terry@strictlywebdev.com",
-            to: "terry@strictlywebdev.com",
+            // to: "terry@strictlywebdev.com",
+            to: {customerEmail},
             subject: "New Stripe Transaction",
             html: `
-                <div style="font-family: Arial, sans-serif;">
+                <div style="font-family: Arial, sans-serif; padding: 0 0 20px 0">
                     <div style="text-align: center;">
                         <img src="https://firebasestorage.googleapis.com/v0/b/dcam-website.appspot.com/o/other_images%2Fg-moon-logo-final.png?alt=media&token=2f9b2309-3021-4eb2-b39f-409bc8370fb2" alt="G Moon Wellness Centre Logo" style="width: 150px;" />
                     </div>
                     <h2 style="background-color: #D6B981; padding: 8px 4px; color: #F3F4F6; margin-top: 0">Thank you for your order!</h2>
-                    <p style="font-style: italic; color: #8C8C8C">Dear ${customerName}, thank you for placing an order with G Moon Wellness! Please see a summary of your order below for your reference:</p>
+                    <small style="color: #8C8C8C">Dear ${customerName}, thank you for placing an order with G Moon Wellness! Please see a summary of your order below for your reference:</small>
                     <hr style="border: none; height: 1px; background-color: #ddd;" />
 
                     <div style="color: #555;">
@@ -76,7 +77,7 @@ export async function POST(req) {
                     </div>
                     <hr style="border: none; height: 1px; background-color: #ddd;" />
 
-                    <p style="color: #8C8C8C">All our appointments take place at our location at 160 East Beaver Creek Rd, #21, Richmond Hill, ON, L4B 3J6. We hope to see you there!</p>
+                    <small style="color: #8C8C8C">All our appointments take place at our location at 160 East Beaver Creek Rd, #21, Richmond Hill, ON, L4B 3J6. We hope to see you there!</small>
                 </div>
             `
         }
