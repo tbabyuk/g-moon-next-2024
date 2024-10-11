74ef463031b78d4db9eb6eb02479044d1e15d68e
Terry Babyuk <tbabyuk@gmail.com>
2024-10-11 00:30:37 -0400

    improved customer email formatting

--- app/api/webhooks/stripe/route.js
+++ app/api/webhooks/stripe/route.js
@@ -45,59 +45,40 @@ export async function POST(req) {
         from: "terry@strictlywebdev.com",
         to: "terry@strictlywebdev.com",
         subject: "New Stripe Transaction",
-        // html: `
-        //         <strong>Amount:</strong><br />
-        //         <small>${amountTotal}</small>
-        //         <hr>
-        //         <strong>Customer Name:</strong><br />
-        //         <small>${customerName}</small>
-        //         <hr>
-        //         <strong>Customer Email:</strong><br />
-        //         <small>${customerEmail}</small>
-        //         <hr>
-        //         <strong>Service Name:</strong><br />
-        //         <small>${serviceName}</small>
-        //         <hr>
-        //         <strong>Service Duration:</strong><br />
-        //         <small>${serviceDuration}</small>
-        //         <hr>
-        //     `
         html: `
             <div style="font-family: Arial, sans-serif; background-color: #F4F4F4; border-radius: 5px;">
-                <div style="background-color: #ffffff; padding: 20px; border-radius: 5px;">
-                    <div style="text-align: center;">
-                        <img src="https://firebasestorage.googleapis.com/v0/b/dcam-website.appspot.com/o/other_images%2Fg-moon-logo-final.png?alt=media&token=2f9b2309-3021-4eb2-b39f-409bc8370fb2" alt="G Moon Wellness Centre Logo" style="width: 150px;" />
-                    </div>
-                    <h2 style="background-color: #D6B981; padding: 8px 4px; color: #F3F4F6; margin-top: 0">Thank you for your order!</h2>
-                    <p style="font-style: italic; color: #8C8C8C">Dear ${customerName}, thank you for placing an order with G Moon Wellness! Please see a summary of your order below for your reference:</p>
-                    <hr style="border: none; height: 1px; background-color: #ddd;" />
-
-                    <div style="color: #555;">
-                        <strong>Service:</strong>
-                        <span style="color: #8C8C8C;">${serviceName}</span>
-                    </div>
-                    <hr style="border: none; height: 1px; background-color: #ddd;" />
-
-                    <div style="color: #555;">
-                        <strong>Appointment Date:</strong>
-                        <span style="color: #8C8C8C;">${serviceDate}</span>
-                    </div>
-                    <hr style="border: none; height: 1px; background-color: #ddd;" />
-
-                    <div style="color: #555;">
-                        <strong>Appointment Start Time:</strong>
-                        <span style="color: #8C8C8C;">${serviceStartTime}</span>
-                    </div>
-                    <hr style="border: none; height: 1px; background-color: #ddd;" />
-
-                    <div style="color: #555;">
-                        <strong>Appointment Length:</strong>
-                        <span style="color: #8C8C8C;">${serviceDuration}</span>
-                    </div>
-                    <hr style="border: none; height: 1px; background-color: #ddd;" />
-
-                    <p style="color: #8C8C8C">All our appointments take place at our location at 160 East Beaver Creek Rd, #21, Richmond Hill, ON, L4B 3J6. We hope to see you there!</p>
+                <div style="text-align: center;">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/dcam-website.appspot.com/o/other_images%2Fg-moon-logo-final.png?alt=media&token=2f9b2309-3021-4eb2-b39f-409bc8370fb2" alt="G Moon Wellness Centre Logo" style="width: 150px;" />
                 </div>
+                <h2 style="background-color: #D6B981; padding: 8px 4px; color: #F3F4F6; margin-top: 0">Thank you for your order!</h2>
+                <p style="font-style: italic; color: #8C8C8C">Dear ${customerName}, thank you for placing an order with G Moon Wellness! Please see a summary of your order below for your reference:</p>
+                <hr style="border: none; height: 1px; background-color: #ddd;" />
+
+                <div style="color: #555;">
+                    <span>Service:</span>
+                    <span style="font-weight: bold;">${serviceName}</span>
+                </div>
+                <hr style="border: none; height: 1px; background-color: #ddd;" />
+
+                <div style="color: #555;">
+                    <span>Appointment Date:</span>
+                    <span style="font-weight: bold;">${serviceDate}</span>
+                </div>
+                <hr style="border: none; height: 1px; background-color: #ddd;" />
+
+                <div style="color: #555;">
+                    <span>Appointment Start Time:</span>
+                    <span style="font-weight: bold;">${serviceStartTime}</span>
+                </div>
+                <hr style="border: none; height: 1px; background-color: #ddd;" />
+
+                <div style="color: #555;">
+                    <span>Appointment Length:</span>
+                    <span style="font-weight: bold;">${serviceDuration} mins</span>
+                </div>
+                <hr style="border: none; height: 1px; background-color: #ddd;" />
+
+                <p style="color: #8C8C8C">All our appointments take place at our location at 160 East Beaver Creek Rd, #21, Richmond Hill, ON, L4B 3J6. We hope to see you there!</p>
             </div>
         `
     }
