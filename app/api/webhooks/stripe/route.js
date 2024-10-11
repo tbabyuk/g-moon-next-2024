3925b30d549258d7e65a9c97fb4f34b8e3865c85
Terry Babyuk <tbabyuk@gmail.com>
2024-10-11 00:40:49 -0400

    further tweaked customer webhook email

--- app/api/webhooks/stripe/route.js
+++ app/api/webhooks/stripe/route.js
@@ -1,4 +1,4 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server"
 import nodemailer from "nodemailer"
 
 
@@ -18,17 +18,15 @@ export async function POST(req) {
     const serviceDuration = requestBody.data.object.metadata.chosenDuration
 
 
-    // const userClerkId = requestBody.data.object.metadata.userClerkId;
+    // console.log("logging whole requestBody from Stripe webhook:", requestBody)
+    // console.log("amount total:", amountTotal)
+    // console.log("customer email:", customerEmail)
+    // console.log("customer name:", customerName)
+    // console.log("service name:", serviceName)
+    // console.log("service duration:", serviceDuration)
 
-        // console.log("logging whole requestBody from Stripe webhook:", requestBody)
-        // console.log("amount total:", amountTotal)
-        // console.log("customer email:", customerEmail)
-        // console.log("customer name:", customerName)
-        // console.log("service name:", serviceName)
-        // console.log("service duration:", serviceDuration)
-
-        // console.log("Logging type and amount:", type, amount)
-        // console.log("logging userClerkId from Stripe Webhook:", userClerkId)
+    // console.log("Logging type and amount:", type, amount)
+    // console.log("logging userClerkId from Stripe Webhook:", userClerkId)
 
 
     const transporter = nodemailer.createTransport({
@@ -40,96 +38,57 @@ export async function POST(req) {
     })
 
 
-    const emailOptions =
-    {
-        from: "terry@strictlywebdev.com",
-        to: "terry@strictlywebdev.com",
-        subject: "New Stripe Transaction",
-        html: `
-            <div style="font-family: Arial, sans-serif; background-color: #F4F4F4; border-radius: 5px;">
-                <div style="text-align: center;">
-                    <img src="https://firebasestorage.googleapis.com/v0/b/dcam-website.appspot.com/o/other_images%2Fg-moon-logo-final.png?alt=media&token=2f9b2309-3021-4eb2-b39f-409bc8370fb2" alt="G Moon Wellness Centre Logo" style="width: 150px;" />
+    const mailOptions =
+        {
+            from: "terry@strictlywebdev.com",
+            to: "terry@strictlywebdev.com",
+            subject: "New Stripe Transaction",
+            html: `
+                <div style="font-family: Arial, sans-serif;">
+                    <div style="text-align: center;">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/dcam-website.appspot.com/o/other_images%2Fg-moon-logo-final.png?alt=media&token=2f9b2309-3021-4eb2-b39f-409bc8370fb2" alt="G Moon Wellness Centre Logo" style="width: 150px;" />
+                    </div>
+                    <h2 style="background-color: #D6B981; padding: 8px 4px; color: #F3F4F6; margin-top: 0">Thank you for your order!</h2>
+                    <p style="font-style: italic; color: #8C8C8C">Dear ${customerName}, thank you for placing an order with G Moon Wellness! Please see a summary of your order below for your reference:</p>
+                    <hr style="border: none; height: 1px; background-color: #ddd;" />
+
+                    <div style="color: #555;">
+                        <span>Service:</span>
+                        <span style="font-weight: bold;">${serviceName}</span>
+                    </div>
+                    <hr style="border: none; height: 1px; background-color: #ddd;" />
+
+                    <div style="color: #555;">
+                        <span>Appointment Date:</span>
+                        <span style="font-weight: bold;">${serviceDate}</span>
+                    </div>
+                    <hr style="border: none; height: 1px; background-color: #ddd;" />
+
+                    <div style="color: #555;">
+                        <span>Appointment Start Time:</span>
+                        <span style="font-weight: bold;">${serviceStartTime}</span>
+                    </div>
+                    <hr style="border: none; height: 1px; background-color: #ddd;" />
+
+                    <div style="color: #555;">
+                        <span>Appointment Length:</span>
+                        <span style="font-weight: bold;">${serviceDuration} mins</span>
+                    </div>
+                    <hr style="border: none; height: 1px; background-color: #ddd;" />
+
+                    <p style="color: #8C8C8C">All our appointments take place at our location at 160 East Beaver Creek Rd, #21, Richmond Hill, ON, L4B 3J6. We hope to see you there!</p>
                 </div>
-                <h2 style="background-color: #D6B981; padding: 8px 4px; color: #F3F4F6; margin-top: 0">Thank you for your order!</h2>
-                <p style="font-style: italic; color: #8C8C8C">Dear ${customerName}, thank you for placing an order with G Moon Wellness! Please see a summary of your order below for your reference:</p>
-                <hr style="border: none; height: 1px; background-color: #ddd;" />
-
-                <div style="color: #555;">
-                    <span>Service:</span>
-                    <span style="font-weight: bold;">${serviceName}</span>
-                </div>
-                <hr style="border: none; height: 1px; background-color: #ddd;" />
-
-                <div style="color: #555;">
-                    <span>Appointment Date:</span>
-                    <span style="font-weight: bold;">${serviceDate}</span>
-                </div>
-                <hr style="border: none; height: 1px; background-color: #ddd;" />
-
-                <div style="color: #555;">
-                    <span>Appointment Start Time:</span>
-                    <span style="font-weight: bold;">${serviceStartTime}</span>
-                </div>
-                <hr style="border: none; height: 1px; background-color: #ddd;" />
-
-                <div style="color: #555;">
-                    <span>Appointment Length:</span>
-                    <span style="font-weight: bold;">${serviceDuration} mins</span>
-                </div>
-                <hr style="border: none; height: 1px; background-color: #ddd;" />
-
-                <p style="color: #8C8C8C">All our appointments take place at our location at 160 East Beaver Creek Rd, #21, Richmond Hill, ON, L4B 3J6. We hope to see you there!</p>
-            </div>
-        `
-    }
-
+            `
+        }
 
 
         try {
-            // throw new Error("Ooops, error with register form!!!! OOPS")
-            await transporter.sendMail(emailOptions);
-            console.log("try block fired in Node.js")
+            await transporter.sendMail(mailOptions);
             // return NextResponse.json({message: "new registration email sent successfully"}, {status: 200})
         } catch (error) {
             console.log("error in catch black on server:", error.message)
             // return NextResponse.json({message: error.message}, {status: 500})
         }
-
-
-        // let numCreditsPurchased;
-
-        // if(type === "charge.succeeded") {
-
-        //     switch(amount) {
-        //         case 500:
-        //             numCreditsPurchased = 20;
-        //             break;
-        //         case 1000:
-        //             numCreditsPurchased = 50;
-        //             break;
-        //         case 1500:
-        //             numCreditsPurchased = 100;
-        //             break;
-        //     }
-
-        //     console.log("logging number of credits purchased:", numCreditsPurchased)
-
-        //     try {
-        //         await connectToESLWorksheetWizardDB()
     
-        //         // add new user to database
-        //         // await User.updateOne({ userClerkId }, {userApiCount: userApiCount + numCreditsPurchased})
-        //         await User.updateOne({ userClerkId }, { $inc: { userApiCount: numCreditsPurchased } })
-            
-        //         // return new Response(JSON.stringify(result), {status: 200})
-        //         console.log("credits update successful!")
-        //         return NextResponse.json({message: "Update Successful!"}, {status: 200})
-        
-        //     } catch (error) {
-        //         // return new Response("Failed to fetch students", {status: 500})
-        //         return NextResponse.json({message: error.message}, {status: 500})
-        //     }
-        // }
-    
-    return NextResponse.json({message: "success"})
+        return NextResponse.json({message: "success"})
 }
