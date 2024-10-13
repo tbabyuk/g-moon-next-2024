776bc9a809d5c4330344096b43bea8c0131f0313
Terry Babyuk <tbabyuk@gmail.com>
2024-10-13 00:21:38 -0400

    updated ServiceCard styling

--- app/components/ServiceCard.jsx
+++ app/components/ServiceCard.jsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 export const ServiceCard = ({service}) => {
 
     return(
-        <Link href={service.url} className="h-[180px] w-1/4 min-w-[250px] g-moon-service-icon hover:cursor-pointer rounded-md">
+        <Link href={service.url} className="h-[180px] w-1/4 min-w-[250px] g-moon-service-card">
             <div className="h-[70%] flex justify-center items-center">
                 <img src={service.icon} className="h-[70%]" alt="" />
             </div>
