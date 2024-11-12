4e59276887524fcd674ea19fc47b4f46fd305701
Terry Babyuk <tbabyuk@gmail.com>
2024-11-11 23:03:48 -0500

    improved popup banner bg styling, fixed hero image sizing

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
     return(
         <div className="min-h-[calc(100vh-115px)] bg-[url('/hero-bg.jpg')] bg-center bg-cover grid grid-cols-1 lg:grid-cols-2">
             <div className="flex pt-12 lg:pt-0 justify-center items-center">
-                <img src="/g-moon-header-image-500.png" className="w-[60%] max-w-[500px]" alt="Relaxing Massage" />
+                <img src="/g-moon-header-image-500.png" className="min-w-[300px] max-w-[450px]" alt="Relaxing Massage" />
             </div>
             <div className="flex flex-col items-center lg:items-start text-g-moon-white px-6 md:px-12 py-12 md:py-20">
                 <h2 className="text-5xl md:text-6xl mb-10 font-medium text-center lg:text-start">Rejuvenate your body, mind and soul</h2>
